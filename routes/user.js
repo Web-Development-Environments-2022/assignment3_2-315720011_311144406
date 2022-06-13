8abbccfa20c88481cfff5c3e925dc9de99d2784d
omermaim <omermaimon2@gmail.com>
2022-06-13 18:16:25 +0300

    finined 3 random recipes

--- routes/user.js
+++ routes/user.js
@@ -51,6 +51,19 @@ router.get('/favorites', async (req,res,next) => {
     next(error); 
   }
 });
+router.get('/myrecipes', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    let myrecipes = {};
+    const recipes_id = await user_utils.getFavoriteRecipes(user_id);
+    let recipes_id_array = [];
+    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+    const results = await recipe_utils.getRecipesPreview(recipes_id_array);
+    res.status(200).send(results);
+  } catch(error){
+    next(error); 
+  }
+});
 
 
 
